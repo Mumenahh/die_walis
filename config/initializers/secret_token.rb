06be81e91c1dee8f158756a5cda4bfe51bc32d78
Markus Alpers <Markus.Alpers@haw-hamburg.de>
2011-04-22 14:07:54 +0200

    Initialisierung des Projekts Die Walis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DieWalis::Application.config.secret_token = 'a048a912851b466c45ad2fabdbe4d429ee5928bce65ea628ee044a7102e75f2114c7fe8030cf3ae155b8214dbfa34e6437f8125e1b45712eda21113fc42b5855'
